949054c33344cfd9817037cdfa63cb00dc5910fe
Crivetkaaa <crivetkaa@vk.com>
2025-03-27 20:51:05 +0300

    Всё сделано(пока что)

--- SmartGardenESP.ino
+++ SmartGardenESP.ino
@@ -1,235 +1,211 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
-#include <ArduinoJson.h>
+
+
 #include <DHT.h>
-#include <EEPROM.h>
+#include <DHT_U.h>
+
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+#include "EEPROM.h"
 
 #define DHTPIN 4
-
-DHT dht(DHTPIN, DHT22);
 
 const int relayPin1 = D6;
 const int relayPin2 = D7;
 const int relayPin3 = D8;
 
-// Настройки точки доступа
-String WiFiSSID = "";
-String WiFipassword = "";
+DHT dht(DHTPIN, DHT22);
+ESP8266WebServer server(80);
+HTTPClient http;
+WiFiClient client;
 
-const char* IP = "188.32.24.142:12000";
 
-const char* ssid = "ESP8266";
-const char* password = "123456789";
+const char* IP = "192.168.0.24:8000";
 
-int user_id;
-const char* ardiono_name;
-const char* arduino_description;
+bool ArduinoIn = false;
+bool start = false;
+
+
+float air_te;
+float air_h;
+float earth_h;
+
 int arduino_id;
 
 
-ESP8266WebServer server(80);
 
-void handlePost() {
-  // Проверяем, есть ли тело запроса
-  if (server.hasArg("plain")) {
-    // Получаем тело запроса
-    String json = server.arg("plain");
-
-    // Создаем объект для разбора JSON
-    JsonDocument doc;
-
-    // Разбираем JSON
-    DeserializationError error = deserializeJson(doc, json);
-
-    // Проверяем на ошибки
-    if (error) {
-      Serial.print(F("Ошибка разбора JSON: "));
-      Serial.println(error.f_str());
-      server.send(400, "application/json", "{\"error\":\"Invalid JSON\"}");
-      return;
-    }
-
-    // Извлекаем значения
-    const char* wifiSSID = doc["wifiSSID"];
-    const char* wifiPassword = doc["wifiPassword"];
-    ardiono_name = doc["ardiono_name"];
-    arduino_description = doc["arduino_description"];
-    user_id = doc["user_id"];
-
-    // Выводим значения на Serial Monitor
-    Serial.println("Полученные данные:");
-    Serial.println(wifiSSID);
-    Serial.println(wifiPassword);
-    Serial.println(ardiono_name);
-    Serial.println(arduino_description);
-    Serial.println(user_id);
-
-
-    WiFi.begin(wifiSSID, wifiPassword);
-    unsigned long starttime = millis();
-    unsigned long endtime = millis();
-
-    while (WiFi.status() != WL_CONNECTED && endtime-starttime <=30000) {
-      delay(500);
-      Serial.print(".");
-      endtime = millis();
-
-
-    }
-
-    Serial.print("Подключение к Wi-Fi");
-    // Отправляем ответ клиенту
-    server.send(200, "application/json", "{\"status\":\"True\"}");
-    String WiFiData = String(wifiSSID) + "_" + String(wifiPassword);
-    int len = WiFiData.length();
-    for (int i=0; i<len; i++){
-      EEPROM.write(i, char(WiFiData[i]));
-    }
-    EEPROM.write(len, '\0');
-    EEPROM.commit();
-    Serial.println(WiFiData);
-    sendArduinoData();
-
-  } else {
-    server.send(400, "application/json", "{\"error\":\"No data\"}");
+void writeEEPROM(String wifi_password){
+  int len = wifi_password.length();
+  for(int i=0; i<len; i++){
+    EEPROM.write(i, wifi_password[i]);
   }
+  EEPROM.write(len, '\0');
+  EEPROM.commit();
 }
 
-void sendArduinoData(){
-  if (WiFi.status() == WL_CONNECTED) { // Проверяем, подключены ли мы к Wi-Fi
-    
-    WiFi.softAPdisconnect(true);
-    
-    String WiFiData = "";
-    
-
-     WiFiClient client;
-        HTTPClient http;
-    http.begin(client, String("http://") + IP + "/api/arduino/");
-    http.addHeader("Content-Type", "application/json");
-
-    String mac_address = WiFi.macAddress();
-
-    JsonDocument doc;
-    Serial.print("id пользователя: ");
-    Serial.println(user_id);
-    doc["user_id"] = user_id;
-    doc["arduino_name"] = ardiono_name;
-    doc["arduino_description"] = arduino_description;
-    doc["mac_address"] = mac_address;
-
-    String jsonString;
-    serializeJson(doc, jsonString);
-
-    int httpResponseCode = http.POST(jsonString);
-    if (httpResponseCode > 0) {
-            String response = http.getString();
-            Serial.println("Ответ от сервера: " + response);
-            JsonDocument doc;
-            DeserializationError error = deserializeJson(doc, response);
-
-      if (error) {
-        Serial.print(F("Ошибка разбора JSON: "));
-        Serial.println(error.f_str());
-        return;
-      }
-      arduino_id = doc["arduino_id"];
-      } else {
-        Serial.print("Ошибка при отправке запроса: ");
-        Serial.println(httpResponseCode);
-        }
-    http.end();
-
-  }
-}
-
-
-void getWiFiData(){
-
+void readEEPROM(){
   Serial.println("Get Wifi data");
   String SSID_and_password = "";
   char breaker;
   int i = 0;
 
-  // Чтение данных из EEPROM
   while (true) {
-    Serial.println(char(EEPROM.read(i)));
     breaker = EEPROM.read(i);
-    
-    // Проверка на завершение строки
     if (breaker == '\0') {
-      break; // Выход из цикла, если достигнут завершающий нуль
+      break;
     }
-    
     SSID_and_password += breaker; 
-    
     i++;
   }
-  String ssid = "";
-  String pass = "";
+  
   int separatorIndex = SSID_and_password.indexOf('_'); // Находим индекс разделителя
-
-  Serial.println(SSID_and_password);
+  String WiFiSSID;
+  String WiFipassword;
   if (separatorIndex != -1) {
     WiFiSSID = SSID_and_password.substring(0, separatorIndex); // Получаем SSID
     WiFipassword = SSID_and_password.substring(separatorIndex + 1); // Получаем пароль
   }
   
   if (!WiFiSSID.isEmpty()){
-    Serial.println("Wifi: " + WiFiSSID);
+    Serial.println("\nWifi: " + WiFiSSID);
     Serial.println("Password: " + WiFipassword);
 
     WiFi.begin(WiFiSSID, WiFipassword);
     unsigned long starttime = millis();
-    unsigned long endtime = millis();
 
-    while (WiFi.status() != WL_CONNECTED && endtime-starttime <=30000) {
+    while (WiFi.status() != WL_CONNECTED && millis()-starttime <=30000) {
       delay(500);
       Serial.print(".");
-      endtime = millis();
       }
+    WiFi.softAPdisconnect(true);
   }
-
-
 }
 
-void setup() {
-  Serial.begin(115200);
-  dht.begin();
-  EEPROM.begin(512);
-  Serial.println("Start");
-  getWiFiData();
-  if(WiFi.status() != WL_CONNECTED){
-  // Настраиваем точку доступа
-    WiFi.softAP(ssid, password);
-    Serial.println("\nТочка доступа запущена");
+void startServer(){
+  WiFi.softAP("ESP8266", "123456789");
+  Serial.println("\nТочка доступа запущена");
 
-    // Устанавливаем обработчик для POST-запроса
-    server.on("/newarduino/", HTTP_POST, handlePost);
+  // Устанавливаем обработчик для POST-запроса
+  server.on("/newarduino/", HTTP_POST, handlePost);
 
-    // Запускаем сервер
-    server.begin();
-    Serial.println("HTTP сервер запущен");
-    randomSeed(analogRead(0));
-  } else {
-    Serial.println("else");
-    WiFiClient client;
-    HTTPClient http;
-    String url = String("http://") + IP + "/api/getformac/?mac_address=" + String(WiFi.macAddress());
-    
-    http.begin(client, url);
+  // Запускаем сервер
+  server.begin();
+  Serial.println("HTTP сервер запущен");
+  randomSeed(analogRead(0));
+}
 
-    JsonDocument doc;
+void getArduino(){
+  Serial.println("else");
+  
+  JsonDocument doc;
 
-    int ResponseStatus = http.GET();
-    if (ResponseStatus > 0) {
+  String url = String("http://") + IP + "/api/getformac/?mac_address=" + String(WiFi.macAddress());
+  http.begin(client, url);
+
+  int ResponseStatus = http.GET();
+  if (ResponseStatus > 0) {
     String response = http.getString();
     Serial.println(response);
     DeserializationError error = deserializeJson(doc, response);
-    arduino_id = doc["arduino_id"];
-
+    if (doc["arduino_id"] != -1) {
+      arduino_id = doc["arduino_id"];
+      ArduinoIn = true;
+      start = true;
+    } else {
+      startServer();
     }
+  }
+}
+
+void handlePost(){
+  if (server.hasArg("plain")) {
+    String json = server.arg("plain");
+
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, json);
+
+    const char* wifiSSID = doc["wifiSSID"];
+    const char* wifiPassword = doc["wifiPassword"];
+    const char* arduino_name = doc["arduino_name"];
+    const char* arduino_description = doc["arduino_description"];
+    int user_id = doc["user_id"];
+
+    // Выводим значения на Serial Monitor
+    Serial.println("Полученные данные:");
+    Serial.println(wifiSSID);
+    Serial.println(wifiPassword);
+    Serial.println(arduino_name);
+    Serial.println(arduino_description);
+    Serial.println(user_id);
+
+
+    WiFi.begin(wifiSSID, wifiPassword);
+    unsigned long starttime = millis();
+
+    while (WiFi.status() != WL_CONNECTED && millis()-starttime <=30000) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.print("Подключение к Wi-Fi\n");
+    // Отправляем ответ клиенту
+    server.send(200, "application/json", "{\"status\":\"True\"}");
+    delay(1000);
+    WiFi.softAPdisconnect(true);
+
+    String WiFiData = String(wifiSSID) + "_" + String(wifiPassword);
+    writeEEPROM(WiFiData);
+    Serial.println(WiFiData);
+    sendArduinoData(arduino_name, arduino_description, user_id);
+
+  } else {
+    server.send(400, "application/json", "{\"error\":\"No data\"}");
+  }
+}
+
+void sendArduinoData(String arduino_name, String arduino_description, int user_id){
+  if (WiFi.status() == WL_CONNECTED){
+    
+    JsonDocument doc;
+
+    String url = String("http://") + IP + "/api/arduino/";
+    http.begin(client, url);
+    http.addHeader("Content-Type", "application/json");
+
+    doc["user_id"] = user_id;
+    doc["arduino_name"] = arduino_name;
+    doc["arduino_description"] = arduino_description;
+    doc["mac_address"] = String(WiFi.macAddress());
+
+    String jsonString;
+    serializeJson(doc, jsonString);
+    int httpResponseCode = http.POST(jsonString);
+    if (httpResponseCode > 0){
+      String response = http.getString();
+      Serial.println("Ответ от сервера: " + response);
+      JsonDocument doc;
+      deserializeJson(doc, response);
+      if (doc["arduino_id"] != -1){
+        arduino_id = doc["arduino_id"];
+        ArduinoIn = true;
+        start = true;
+      }
+    }
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115000);
+  EEPROM.begin(512);
+  dht.begin();
+  readEEPROM();
+
+  if (WiFi.status() != WL_CONNECTED){
+    startServer();
+  } else {
+    getArduino();
   }
 
   pinMode(relayPin1, OUTPUT);
@@ -238,29 +214,68 @@ void setup() {
   digitalWrite(relayPin2, HIGH);
   pinMode(relayPin3, OUTPUT);
   digitalWrite(relayPin3, HIGH);
-
 }
 
 unsigned long start_time = 0;
 unsigned long check_time = 0;
+unsigned long check_status = 0;
+
+bool ArduinoWork = false;
+
+
+void getArduinoStatus(){
+  JsonDocument doc;
+  
+  String url = String("http://") + IP + "/api/checkstatus/?arduino_id=" + arduino_id;
+  http.begin(client, url);
+
+  int httpResponse = http.GET();
+  if (httpResponse > 0) {
+    String response = http.getString();
+    deserializeJson(doc, response);
+    ArduinoWork = doc["status"];
+
+    if (!ArduinoWork) {
+      url += "&second=true";
+      http.begin(client, url);
+      int httpResponse = http.GET();
+      if (httpResponse > 0) {
+        String response = http.getString();
+        deserializeJson(doc, response);
+        ArduinoIn = doc["status"];
+        if (!ArduinoIn && start){
+          startServer();
+          start = false;
+        }
+      }
+    }
+  }
+}
 
 void loop() {
-    server.handleClient();
-    if (millis() - start_time > 60000 && WiFi.status() == WL_CONNECTED) {
-      start_time = millis();
-      sendData();
-    }
-    if (millis() - check_time > 1000 && WiFi.status() == WL_CONNECTED){
-      check_time = millis();
-      getReleStatus();
-    }
+  // put your main code here, to run repeatedly:
 
+  server.handleClient();
+
+  if(millis() - check_status > 5000 && WiFi.status() == WL_CONNECTED){
+    getArduinoStatus();
+    check_status = millis();
+  }
+
+  if (millis() - start_time > 60000 && WiFi.status() == WL_CONNECTED && ArduinoWork) {
+    start_time = millis();
+    sendData();
+  }
+
+  if (millis() - check_time > 1000 && WiFi.status() == WL_CONNECTED && ArduinoWork){
+    check_time = millis();
+    getReleStatus();
+  }
 }
+
 
 void getReleStatus(){
   if (WiFi.status() == WL_CONNECTED) { // Проверяем, подключены ли мы к Wi-Fi
-    WiFiClient client;
-    HTTPClient http;
     String url = String("http://") + IP + "/api/relestatus/?arduino_id=" + String(arduino_id);
     http.begin(client,  url);
     
@@ -288,7 +303,6 @@ void getReleStatus(){
         digitalWrite(relayPin3, HIGH);
       } else {digitalWrite(relayPin3, LOW);}
 
-
     } else {
       String response = http.getString();
       Serial.print("RELE Ошибка от сервера: ");
@@ -297,21 +311,13 @@ void getReleStatus(){
   }
 }
 
-float air_te;
-float air_h;
-float earth_h;
-
 void getData(){
   air_te = dht.readTemperature();
   air_h = dht.readHumidity();
-
-
 }
 
 void sendData(){
   if (WiFi.status() == WL_CONNECTED) { // Проверяем, подключены ли мы к Wi-Fi
-    WiFiClient client;
-    HTTPClient http;
     getData();
     http.begin(client,  String("http://") + IP + "/api/data/");
     http.addHeader("Content-Type", "application/json");
@@ -333,7 +339,6 @@ void sendData(){
       Serial.print("Ошибка от сервера: ");
       Serial.println(response);
     }
-    
-
   }
 }
+
